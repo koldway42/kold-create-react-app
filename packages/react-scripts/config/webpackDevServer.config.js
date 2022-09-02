7f19dcd3d4ce3a7071649ba8c879a952c3e0048e
koldway42 <math12125.jr@gmail.com>
2022-09-02 01:01:27 -0300

    Config change

--- packages/react-scripts/config/webpackDevServer.config.js
+++ packages/react-scripts/config/webpackDevServer.config.js
@@ -91,13 +91,14 @@ module.exports = function (proxy, allowedHost) {
         warnings: false,
       },
     },
-    writeToDisk: true,
     devMiddleware: {
       // It is important to tell WebpackDevServer to use the same "publicPath" path as
       // we specified in the webpack config. When homepage is '.', default to serving
       // from the root.
       // remove last slash so user can land on `/test` instead of `/test/`
       publicPath: paths.publicUrlOrPath.slice(0, -1),
+      //Write desenv files to disk
+      writeToDisk: true
     },
 
     https: getHttpsConfig(),
